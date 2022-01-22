7380ca06ba7a32e7ca023f1d5baaa8b8a86ce521
macha-dev <chastonmax@gmail.com>
2022-01-22 05:43:13 +0000

    Initial commit

---
+++ src/main/java/uk/wimble/uhc/Main.java
@@ -0,0 +1,11 @@
+package uk.wimble.uhc;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin {
+    @Override
+    public void onEnable() {};
+
+    @Override
+    public void onDisable() {};
+}
